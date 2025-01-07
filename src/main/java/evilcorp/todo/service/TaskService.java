286989ce9ab7e21fb11ce8e9e9737d8e20cbc9e1
chrxmehearts <sengvihss@gmail.com>
2025-01-07 21:58:24 +0100

    end task service

--- src/main/java/evilcorp/todo/service/TaskService.java
+++ src/main/java/evilcorp/todo/service/TaskService.java
@@ -1,7 +1,27 @@
 package evilcorp.todo.service;
 
+import evilcorp.todo.entity.Task;
+import evilcorp.todo.entity.User;
+import evilcorp.todo.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task addTask(Task task) {
+        return taskRepository.save(task);
+    }
+    public List<Task> getTasksByUser(User user) {
+        return taskRepository.findByUser(user);
+    }
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
 }
