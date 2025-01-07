645645946c02dbcdf2b9a3012cce2ca3476f8770
chrxmehearts <sengvihss@gmail.com>
2025-01-07 21:41:11 +0100

    Finish task entity

---
+++ src/main/java/evilcorp/todo/entity/Task.java
@@ -0,0 +1,26 @@
+package evilcorp.todo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column
+    private String description;
+    @Column
+    private boolean completed;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
