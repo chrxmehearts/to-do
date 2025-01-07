b3e3c5c3a1c0aca7ab7859cad2cdb370fe71459a
chrxmehearts <sengvihss@gmail.com>
2025-01-07 22:39:07 +0100

    SecurityConfig

--- src/main/java/evilcorp/todo/config/SecurityConfig.java
+++ src/main/java/evilcorp/todo/config/SecurityConfig.java
@@ -5,31 +5,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
     private final UserService userService;
 
     public SecurityConfig(UserService userService) {
         this.userService = userService;
     }
-
-    protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers("/api/users/register", "/login").permitAll()
-                                .anyRequest()
-                                .authenticated())
-                .formLogin(formLogin -> formLogin.loginPage("/login").permitAll())
-                .logout(LogoutConfigurer::permitAll);
-    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/api/users/register", "/login").permitAll().anyRequest().authenticated()).formLogin(formLogin -> formLogin.loginPage("/login").permitAll()).logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
 }
