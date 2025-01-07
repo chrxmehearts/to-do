c959df2712717871b86c478cc61442b47e78c13d
chrxmehearts <sengvihss@gmail.com>
2025-01-07 21:55:56 +0100

    end user service

--- src/main/java/evilcorp/todo/service/TaskService.java
+++ src/main/java/evilcorp/todo/service/TaskService.java
@@ -1,4 +1,7 @@
 package evilcorp.todo.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskService {
 }
--- src/main/java/evilcorp/todo/service/UserService.java
+++ src/main/java/evilcorp/todo/service/UserService.java
@@ -1,4 +1,24 @@
 package evilcorp.todo.service;
 
+import evilcorp.todo.entity.User;
+import evilcorp.todo.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findUserByUsername(String username) {
+        return Optional.ofNullable(userRepository.findByUsername(username));
+    }
 }
