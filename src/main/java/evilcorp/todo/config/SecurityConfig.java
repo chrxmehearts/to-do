84b4a322b04359683beb0a80a60ee0900aa8d7d0
chrxmehearts <sengvihss@gmail.com>
2025-01-07 22:24:31 +0100

    setup security config

---
+++ src/main/java/evilcorp/todo/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package evilcorp.todo.config;
+
+import evilcorp.todo.service.UserService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.requestMatchers("/api/users/register", "/login").permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .formLogin(formLogin -> formLogin.loginPage("/login").permitAll())
+                .logout(LogoutConfigurer::permitAll);
+    }
+}
