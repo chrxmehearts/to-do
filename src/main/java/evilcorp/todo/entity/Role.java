c4e5f7a4ced2adced76a60091ede28f30a8833f1
chrxmehearts <sengvihss@gmail.com>
2025-01-09 21:13:57 +0100

    add roles entity

--- src/main/java/evilcorp/todo/entity/Role.java
+++ src/main/java/evilcorp/todo/entity/Role.java
@@ -6,19 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "users")
+@Table(name = "roles")
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@NoArgsConstructor
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false, unique = true)
-    private String username;
-    @Column(nullable = false)
-    private String password;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "role_id", nullable = false)
-    private Role role;
+    private String name;
 }
