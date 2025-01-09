c28e04adfd9b0f1a7a5f09e6dc6681b0108be97a
chrxmehearts <sengvihss@gmail.com>
2025-01-09 21:22:18 +0100

    when register your default role is user

--- src/main/java/evilcorp/todo/service/UserService.java
+++ src/main/java/evilcorp/todo/service/UserService.java
@@ -1,6 +1,7 @@
 package evilcorp.todo.service;
 
 import evilcorp.todo.entity.User;
+import evilcorp.todo.repository.RoleRepository;
 import evilcorp.todo.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,14 +11,17 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public User registerUser(User user) {
         user.setPassword(encoder.encode(user.getPassword()));
+        user.setRole(roleRepository.findByName("user"));
         return userRepository.save(user);
     }
 
