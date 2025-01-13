c94d2c0e89791431ec0d44204989cbf2f18e2fbb
chrxmehearts <sengvihss@gmail.com>
2025-01-13 20:47:51 +0100

    add users tasks dto

--- src/main/java/evilcorp/todo/DTO/UserTasksAdminDto.java
+++ src/main/java/evilcorp/todo/DTO/UserTasksAdminDto.java
@@ -1,4 +1,16 @@
 package evilcorp.todo.DTO;
 
+import evilcorp.todo.entity.Task;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserTasksAdminDto {
+    private String username;
+    private List<Task> tasks;
 }
