13fd491392e3653afc6e032b11695432c2d322fa
chrxmehearts <sengvihss@gmail.com>
2025-01-16 00:38:17 +0100

    last version

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM openjdk:17-jdk-slim
 
+RUN apt-get update && apt-get install -y wget && \
+    wget https://github.com/jwilder/dockerize/releases/download/v0.6.1/dockerize-linux-amd64-v0.6.1.tar.gz && \
+    tar -xvzf dockerize-linux-amd64-v0.6.1.tar.gz && \
+    mv dockerize /usr/local/bin
+
 WORKDIR /app
 
-COPY target/to-do-0.0.1-SNAPSHOT.jar /app/tasker.jar
+COPY ./target/to-do-0.0.1-SNAPSHOT.jar /app/tasker.jar
 
-ENTRYPOINT ["java", "-jar", "tasker.jar"]
+CMD ["dockerize", "-wait", "tcp://db:3306", "-timeout", "25s", "java", "-jar", "tasker.jar"]
---
+++ src/main/java/evilcorp/todo/service/RoleService.java
@@ -0,0 +1,30 @@
+package evilcorp.todo.service;
+
+import evilcorp.todo.entity.Role;
+import evilcorp.todo.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Transactional
+    public void addRolesIfNotExist() {
+        if (!roleRepository.existsByName("ROLE_ADMIN")) {
+            Role adminRole = new Role();
+            adminRole.setName("ROLE_ADMIN");
+            roleRepository.save(adminRole);
+        }
+
+        if (!roleRepository.existsByName("ROLE_USER")) {
+            Role userRole = new Role();
+            userRole.setName("ROLE_USER");
+            roleRepository.save(userRole);
+        }
+    }
+}
+
